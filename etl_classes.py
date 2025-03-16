a50aa71f7d553d84063a8f7cb226b86929e813a9
Aleks Zakrzewski <alexzakr@gmail.com>
2025-03-16 20:15:53 +0100

    basic classes

--- etl_classes.py
+++ etl_classes.py
@@ -1,29 +1,62 @@
+import csv
+import pandas as pd
+from pandas import DataFrame
+
 class CsvExtractor():
-    def extract(extract, path):
-        pass
+    def __init__(self, path):
+        self.path = path
+    def extract(self) -> list[list]:
+        with open(file=self.path, mode='r') as f:
+            file_data = csv.reader(f, delimiter=',')
+            data_table = []
+            for row in file_data:
+                data_table.append(row)
+        return data_table
 
 
 class Deduplicator():
-    def transform(data: list[list]) -> list[list]:
-        pass
+    def __init__(self, data_table):
+        self.data_table = data_table
+    def transform(self) -> list[list]:
+        add_data = self.data_table[1::]
+        return self.data_table + add_data 
 
 
 class JsonLoader():
-    def __init__(self, orient, index, lines):
-        pass
-
-    def load(path):
-        pass
+    def __init__(self,
+                 orient: str,
+                 index: bool,
+                 lines: bool):
+        self.orient = orient
+        self.index = index
+        self.lines = lines
+    def load(self, df: DataFrame, path: str) -> None:
+        df.to_json(
+            path, 
+            orient=self.orient,
+            index=self.index,
+            lines=self.lines
+            )
 
 
 class Job():
-    def __init__(
-        self,
-        input_path: str,
-        output_path: str,
-        extractor: CsvExtractor,
-        dedubl: Deduplicator,
-        json_loader: JsonLoader
-        ):
-        def run(self):
-            pass
+    def __init__(self,
+                 input_path: str,
+                 output_path: str,
+                 json_loader: JsonLoader) -> None:
+        self.file_path = input_path
+        self.output_path = output_path
+        self.json_loader = json_loader
+
+    def run(self):
+        csv_data = CsvExtractor(path=self.file_path)
+        data_table = csv_data.extract()
+        transformer = Deduplicator(data_table=data_table)
+        transformed_data = transformer.transform()
+        df = pd.DataFrame(
+            data=transformed_data[1::],
+            columns=transformed_data[0]
+        )
+        self.json_loader.load(df=df, path=self.file_path)
+        
+    
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import etl_classes
 
+file_input_path = 'pracownicy.csv'
+file_output_path = 'pracownicy.json'
 
-etl_process = etl_classes.Job()
+def main():
 
-etl_process.
+    json_loader = etl_classes.JsonLoader(
+        orient="records",
+        index=False,
+        lines=True)
+
+    etl_job = etl_classes.Job(
+        input_path=file_input_path,
+        output_path=file_output_path,
+        json_loader=json_loader
+    )
+
+    etl_job.run()
+
+if __name__ == "__main__":
+    main()
