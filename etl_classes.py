f028d804b6caa060271b5357ae2265f0a2f4fc6b
Aleks Zakrzewski <alexzakr@gmail.com>
2025-03-16 16:20:10 +0100

    setted base classes

---
+++ etl_classes.py
@@ -0,0 +1,26 @@
+class CsvExtractor():
+    def extract(extract, path):
+        pass
+
+
+class Deduplicator():
+    def transform(data: list[list]) -> list[list]:
+        pass
+
+
+class JsonLoader():
+    def __init__(self, orient, index, lines):
+        pass
+
+    def load(path):
+        pass
+
+
+class Job():
+    def __init__(
+        self,
+        input_path: str,
+        output_path: str,
+        extractor: CsvExtractor,
+        dedubl: Deduplicator):
+        pass
